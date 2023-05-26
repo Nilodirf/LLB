8c91b24db81921f147c68e0a5b1d894d59d42bef
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2023-05-26 17:46:20 +0200

    asdsad

--- LLB_params.py
+++ LLB_params.py
@@ -7,6 +7,7 @@ from scipy import optimize as op
 from scipy import interpolate as ip
 from matplotlib import pyplot as plt
 import itertools
+import time
 
 # In this file, the dynamical parameters needed for the quantum LLB implementation are computed.
 # We use mean field theory to get the temperature dependence of these parameters. The scaling is mostly defined by the mean field magnetization at a given temperature.
@@ -22,7 +23,7 @@ import itertools
 # (1,2,3,4,5) Let's define a dummy function to construct a sample consisting of different materials (this does not mean atoms but rather a definition for grainsize of at least a unit cell!)
 # For this, I construct a class that holds relevant information for the quantum LLB:
 class material():
-    def __init__(self, name, S, Tc, lamda, muat, kappa_anis, anis_axis, K_0, A_0):
+    def __init__(self, name, S, Tc, lamda, muat, kappa_anis, anis_axis, K_0, A_0, Ms, Delta):
         self.name=name                                      # name of the material used for the string representation of the class
         self.S=S                                            # effective spin
         self.Tc=Tc                                          # Curie temperature
@@ -34,6 +35,8 @@ class material():
         self.anis_axis=anis_axis                            # uniaxials anisotropy axis (x:0, y:1, z:2) other anisotropies are not yet implemented
         self.K_0=K_0                                        # value for the anisotropy at T=0 K in units of J
         self.A_0=A_0                                        # value for the exchange stiffness at T=0 K in units of J*m^2
+        self.Ms=Ms                                          #value for the saturation magnetization at T=0K in J/m^3/T
+        self.Delta=Delta                                    # in-depth length of the magnetic grains
 
     def __str__(self):
         return self.name
@@ -127,15 +130,20 @@ def get_sample():
     #   (iii-v) magnetization amplitudes and angles
 
     # Define define three dummy materials with different parameters:
-    mat_1 = material('uno', 1 / 2, 650., 0.01, 2., 1.6, 2, 1.5e6, 1e-3)
-    mat_2 = material('dos', 7 / 2, 1200., 0.015, 1., 1.6, 2, 1.7e6, 1e-3)
-    mat_3 = material('tres', 1, 300., 0.02, 1.5, 1.6, 0, 2.2e6, 1e-3)
+    mat_1 = material('Nickel', 0.5, 630., 0.02, 0.393, 3, 2,  0.45e6, 1e-11, 500e3, 1e-9)
+    mat_2 = material('Cobalt', 1e6, 1480., 0.1, 0.393, 3, 2, 0.45e6, 1e-11, 1400e3, 1e-9)
+    mat_3 = material('Iron', 2., 900., 0.01, 2.2, 3, 2, 0.45e6, 1e-11, 200e3, 1e-9)
+    # FGT = material ('FGT', 0.5, 220., 0.01, 2.2, 3, 2, 0.45e6, 1e-11, 200e-13, 1e-9)
+    # FGT2 = material ('FGT2', 2., 220., 0.01, 2.2, 3, 2, 0.45e6, 1e-11, 200e-13, 1e-9)
+
     materials = [mat_1, mat_2, mat_3]
 
-    # Define a sample structure where 5 layers of each material build blocks that are periodically stacked 10 times (5*3*10=150=N):
-    building_block = np.concatenate(
-        (np.array([mat_1 for _ in range(5)]), np.array([mat_2 for _ in range(5)]), np.array([mat_3 for _ in range(5)])))
-    sample = np.concatenate([building_block for _ in range(10)])
+    Nickel_1=[mat_1 for _ in range(10)]
+    Cobalt=[mat_2 for _ in range(15)]
+    Iron=[mat_3 for _ in range(10)]
+    Nickel_2=[mat_1 for _ in range(25)]
+
+    sample = np.array(Nickel_1+Cobalt+Iron+Nickel_2)
 
     # The following constructs a list of lists, containing in list[i] a list of indices of material i in the sample_structure. This will help compute the mean field magnetization only once for every material at each timestep.
     material_grain_indices = []
@@ -148,9 +156,9 @@ def get_sample():
     mat_locator = [materials.index(grain) for grain in sample]
 
     # Define initial magnetization on the whole sample (for simplicity uniform) and fully magnetized along the z-axis
-    m_amp = np.ones(150)
-    m_phi = np.zeros(150)
-    m_gamma = np.zeros(150)
+    m_amp = np.ones(1)
+    m_phi = np.zeros(1)
+    m_gamma = np.zeros(1)
     return materials, sample, m_amp, m_phi, m_gamma, material_grain_indices, sample_sorter, mat_locator
 
 
@@ -227,29 +235,28 @@ def get_exch_coup_sample(materials, sample, mat_loc):
     return ex_coup_arr
 
 
-
-def get_ex_stiff_sample(materials, sample, mat_loc):
+def get_ex_stiff_sample(materials, sample, mat_loc, Ms_sam, Delta2_sam):
     # This computes a grid for the exchange stiffness in analogous fashion to get_exch_coup_sam()
     A_mat = np.zeros((len(materials), len(materials)))
     for i, mat in enumerate(materials):
         A_mat[i][i] = mat.A_0
 
-    A_mat[0][1] = 1e-12
-    A_mat[1][2] = 5e-12
-    A_mat[0][2] = 2.5e-12
+    # A_mat[0][1]=1e-11
+    # A_mat[1][2]=5e-11
+    # A_mat[0][2]=2.5e-11
 
     for i in range(1, len(materials)):
         for j in range(i):
             A_mat[i][j] = A_mat[j][i]
 
-    ex_stiff_arr = np.zeros((len(sample), 2))
+    ex_stiff_arr = np.ones((len(sample), 2)) * A_mat[0][0]
 
     for i, grain in enumerate(sample):
         if i > 0:
             ex_stiff_arr[i][0] = A_mat[mat_loc[i]][mat_loc[i - 1]]
         if i < len(sample) - 1:
             ex_stiff_arr[i][1] = A_mat[mat_loc[i]][mat_loc[i + 1]]
-    return ex_stiff_arr
+    return np.divide(ex_stiff_arr, np.multiply(Ms_sam, Delta2_sam)[:, np.newaxis])
 
 def S_sample(sample):
     return np.array([mat.S for mat in sample])
@@ -263,8 +270,17 @@ def J_sample(sample):
 def lamda_sample(sample):
     return np.array([mat.lamda for mat in sample])
 
-def get_ani_sample(sample):
-    ani_sam=np.array([mat.K_0 for mat in sample])
+def Ms_sample(sample):
+    return np.array([mat.Ms for mat in sample])
+
+def Delta2_sample(sample):
+    return np.array([mat.Delta**2 for mat in sample])
+
+def muat_sample(sample):
+    return np.array([mat.muat for mat in sample])
+
+def get_ani_sample(sample, Ms_sam):
+    ani_sam=np.divide(np.array([mat.anisotropy() for mat in sample]), Ms_sam)
     kappa_ani_sam=np.array([mat.kappa_anis for mat in sample])
     ani_perp_sam= np.ones((len(sample), 3))
     for i,mat in enumerate(sample):
@@ -289,23 +305,6 @@ def chi_par_num_sample(sample):
 def chi_par_denomm1_sample(sample):
     return np.array([mat.chi_par_denomm1() for mat in sample])
 
-
-materials, sample, m_amp, m_phi, m_gamma, mat_gr_ind, mat_gr_ind_flat, mat_loc=get_sample()
-plot_mean_mags(materials)
-exch_coup_const_sam=get_exch_coup_sample(materials, sample, mat_loc)
-ex_stiff_sam=get_ex_stiff_sample(materials, sample, mat_loc)
-S_sam=S_sample(sample)
-Tc_sam=Tc_sample(sample)
-J_sam=J_sample(sample)
-lamda_sam=lamda_sample(sample)
-K0_sam, kappa_ani_sam, ani_perp_sam=get_ani_sample(sample)
-alpha_par_sam=alpha_par_sample(sample)
-qs_sam=qs_sample(sample)
-dbrillouin_t1_sam=dbrillouin_t1_sample(sample)
-dbrillouin_t2_sam=dbrillouin_t2_sample(sample)
-chi_par_num_sam=chi_par_num_sample(sample)
-chi_par_denomm1_sam=chi_par_denomm1_sample(sample)
-
 ### Now we deal with the temperature dependence of the parameters. All the following functions are called at every timestep of the dynamical simulation:
 
 
@@ -315,25 +314,18 @@ def split_sample_T(T, tc_mask, mat_gr_ind, materials):
     return T_sep, tc_mask_sep
 
 
-def get_mean_mag_sample_T(mat_gr_ind_flat, materials, T_sep, tc_mask_sep):
+def get_mean_mag_sample_T(mat_gr_ind_flat, materials, T_sep_red, tc_mask_sep):
     Tc_vals = np.array([mat.Tc for mat in materials])
     tc_mask_sep_norm = [np.array(tc_mask) for tc_mask in tc_mask_sep]
-    mean_mags = np.array([mat.get_mean_mag(T, tc_mask) for mat, T, tc_mask in zip(materials, T_sep, tc_mask_sep_norm)])
+    mean_mags = np.array([mat.get_mean_mag(T, tc_mask) for mat, T, tc_mask in zip(materials, T_sep_red, tc_mask_sep_norm)])
     mmag_sam_T_flat = np.concatenate(mean_mags)[mat_gr_ind_flat]
     return mmag_sam_T_flat
 
-Temp_test=np.arange(1,151)*10
-t_reduced = np.divide(Temp_test, [mat.Tc for mat in sample])
-under_tc=t_reduced<1.
-over_tc= ~under_tc
-Temp_sep, tc_mask_sep=split_sample_T(Temp_test, under_tc, mat_gr_ind, materials)
-mmag_sam_T=get_mean_mag_sample_T(mat_gr_ind_flat, materials, Temp_sep, tc_mask_sep)
-
 def ani_sample_T(mmag_sam_T, K0_sam, kappa_ani_sam):
-    return np.multiply(K0_sam,np.power(mmag_sam_T,kappa_ani_sam))
+    return np.multiply(K0_sam,np.power(mmag_sam_T,kappa_ani_sam-2))
 
 def ex_stiff_sample_T(mmag_sam_T, ex_stiff_sam):
-    return np.multiply(np.power(mmag_sam_T[:, np.newaxis],2),ex_stiff_sam)
+    return np.multiply(np.power(mmag_sam_T[:, np.newaxis],2-2),ex_stiff_sam)
 
 def qs_sample_T(qs_sam, mmag_sam_T, T):
     return np.true_divide(np.multiply(qs_sam,mmag_sam_T),T)
@@ -346,27 +338,178 @@ def alpha_par_sample_T(mmag_sam_T, T, alpha_par_sam, qs_sam_T, Tc_sam, under_tc,
 
 def alpha_trans_sample_T(mmag_sam_T, lamda_sam, T, qs_sam_T, Tc_sam, under_tc, over_tc, lambda_sam):
     atsT=np.zeros(len(T))
-    atsT[under_tc]=np.divide(lambda_sam[under_tc], np.multiply(mmag_sam_T[under_tc], np.tanh(qs_sam_T[under_tc])/qs_sam_T[under_tc]-np.divide(mmag_sam_T[under_tc], 3*Tc_sam[under_tc])))
+    atsT[under_tc]=np.multiply(lambda_sam[under_tc], (np.divide(np.tanh(qs_sam_T[under_tc]), qs_sam_T[under_tc])-np.divide(T[under_tc],3*Tc_sam[under_tc])))
     atsT[over_tc]=lambda_sam[over_tc]*2/3*np.divide(T[over_tc], Tc_sam[over_tc])
     return atsT
 
-ani_sam_T=ani_sample_T(mmag_sam_T, K0_sam, kappa_ani_sam)
-ex_stiff_sam_T=ex_stiff_sample_T(mmag_sam_T, ex_stiff_sam)
-qs_sam_T=qs_sample_T(qs_sam, mmag_sam_T, Temp_test)
-alpha_par_sam_T=alpha_par_sample_T(mmag_sam_T, Temp_test, alpha_par_sam, qs_sam_T, Tc_sam, under_tc, over_tc, lamda_sam)
-alpha_trans_sam_T=alpha_trans_sample_T(mmag_sam_T, lamda_sam, Temp_test, qs_sam_T, Tc_sam, under_tc, over_tc, lamda_sam)
+def eta_sample_T(mmag_sam_T, J_sam, T):
+    return J_sam*mmag_sam_T/sp.k/T
 
-def anis_field(anis_sam_T, m, m_squared, ani_perp_sam):
-    ani_strength=anis_sam_T/m_squared
-    return ani_strength[:, np.newaxis]*(m*ani_perp_sam)
+def dbrillouin_sample_T(eta_sam_T, S_sam, dbrillouin_t1_sam, dbrillouin_t2_sam):
+    two_S_sam=2*S_sam
+    x1=np.divide(eta_sam_T,two_S_sam)
+    x2=np.divide(np.multiply(eta_sam_T,(two_S_sam+1)),two_S_sam)
+    sinh_func=1/np.sinh(np.array([x1,x2]))**2
+    dbrillouin_sam_T=dbrillouin_t1_sam*sinh_func[0]-dbrillouin_t2_sam*sinh_func[1]
+    return dbrillouin_sam_T
+
+def chi_par_sample_T(chi_par_num_sam, chi_par_denomm1_sam, dbrillouin_sam_T, T, under_tc, over_tc, muat_sam, Tc_sam, J_sam):
+    cpsT=np.zeros(len(T))
+    cpsT[under_tc]=np.multiply(chi_par_num_sam[under_tc], np.divide(dbrillouin_sam_T, T[under_tc]-np.multiply(chi_par_denomm1_sam[under_tc], dbrillouin_sam_T)))
+    cpsT[over_tc]=np.divide(np.multiply(muat_sam[over_tc]*9.274e-24, Tc_sam[over_tc]), J_sam[over_tc]*(T[over_tc]-Tc_sam[over_tc]+1e-1))
+    return cpsT
+
+
+
+def anis_field(anis_sam_T, m, ani_perp_sam):
+    return anis_sam_T[:, np.newaxis]*(m*ani_perp_sam)
 
 def ex_field(ex_stiff_sam_T, m_diff_up, m_diff_down):
-    return ex_stiff_sam_T[:,0]*m_diff_up+ex_stiff_sam_T[:,1]*m_diff_down
+    return ex_stiff_sam_T[:,0][:,np.newaxis]*m_diff_up+ex_stiff_sam_T[:,1][:,np.newaxis]*m_diff_down
 
 def th_field(m, m_squared, mmag_sam_T, T, Tc_sam, chi_par_sam_T, under_tc, over_tc):
-    factor = 1/2/chi_par_sam_T[under_tc]
+    factor = 1/chi_par_sam_T
     H_th = np.zeros(len(T))
-    H_th[under_tc] = (1-m_squared[under_tc]/mmag_sam_T[under_tc]**2)*factor
-    H_th[over_tc] = (1+3/5*Tc_sam[over_tc]/(T[over_tc]-Tc_sam[over_tc]+1e-6))*m_squared[over_tc]/chi_par_sam_T[over_tc]
+    H_th[under_tc] = (1-m_squared[under_tc]/mmag_sam_T[under_tc]**2)*factor[under_tc]/2
+    H_th[over_tc] = -(1+3/5*Tc_sam[over_tc]/(T[over_tc]-Tc_sam[over_tc]+1e-1))*m_squared[over_tc]*factor[over_tc]
     return H_th[:, np.newaxis]*m
 
+
+delay=np.load('temp_test/delays.npy')
+teNi1=np.load('temp_test/tesNickel0.npy')
+teCo2=np.load('temp_test/tesCobalt1.npy')
+teFe3=np.load('temp_test/tesIron2.npy')
+teNi4=np.load('temp_test/tesNickel3.npy')
+tes=np.append(teNi1, teCo2, axis=1)
+tes=np.append(tes, teFe3, axis=1)
+tes=np.append(tes, teNi4, axis=1)
+tes=np.array([[tes[i,0]] for i in range(len(delay))])*0.7
+
+# paul_dat=open('temp_test/paul_data.txt', 'r').readlines()
+# sep_dat=[line.split() for line in paul_dat]
+# float_dat=np.array([[float(num) for num in line] for line in sep_dat])
+#
+# delay=np.array(float_dat[:,0])[:51]
+# new_delay=np.arange(0,delay[-1], 1e-4)#[:10001]
+
+# tes=float_dat[:,5][:51]
+# new_tes=np.array(ip.interp1d(delay, tes)(new_delay))#[:10001]
+# new_tes = np.reshape(new_tes,(-1,1))
+#
+# mxs= float_dat[:,1]
+# mys=float_dat[:,2]
+# mzs=float_dat[:,3]
+# m2s=float_dat[:,4]
+
+
+def mag_incr(materials, sample, m_amp, m_phi, m_gamma, mat_gr_ind, mat_gr_ind_flat, mat_loc, Ms_sam, ex_stiff_sam,
+             S_sam, Tc_sam, J_sam, lamda_sam, muat_sam, K0_sam, kappa_ani_sam, ani_perp_sam, alpha_par_sam, qs_sam,
+             dbrillouin_t1_sam, dbrillouin_t2_sam, chi_par_num_sam, chi_par_denomm1_sam, m, Te, H_ext, gamma, dt):
+    # some operations of the magnetization we need:
+    m_squared = np.sum(np.power(m, 2), axis=-1)
+    m_diff_down = np.concatenate((np.diff(m, axis=0), np.zeros((1, 3))), axis=0)
+    m_diff_up = -np.roll(m_diff_down, 1)
+
+    # at every timestep, we have to calculate the temperature dependent parameters, so let's call all the functions defined before
+    t_reduced = np.divide(Te, Tc_sam)
+    under_tc = t_reduced < 1.
+    over_tc = ~under_tc
+    Temp_sep, tc_mask_sep = split_sample_T(Te, under_tc, mat_gr_ind, materials)
+    mmag_sam_T = get_mean_mag_sample_T(mat_gr_ind_flat, materials, Temp_sep, tc_mask_sep)
+    anis_sam_T = ani_sample_T(mmag_sam_T, K0_sam, kappa_ani_sam)
+    ex_stiff_sam_T = ex_stiff_sample_T(mmag_sam_T, ex_stiff_sam)
+    qs_sam_T = qs_sample_T(qs_sam, mmag_sam_T, Te)
+    eta_sam_T = eta_sample_T(mmag_sam_T, J_sam, Te)
+    dbrillouin_sam_T = dbrillouin_sample_T(eta_sam_T[under_tc], S_sam[under_tc], dbrillouin_t1_sam[under_tc],
+                                           dbrillouin_t2_sam[under_tc])
+    chi_par_sam_T = chi_par_sample_T(chi_par_num_sam, chi_par_denomm1_sam, dbrillouin_sam_T, Te, under_tc, over_tc,
+                                     muat_sam, Tc_sam, J_sam)
+
+    # from all these we can define the effective field
+    H_ex = ex_field(ex_stiff_sam_T, m_diff_up, m_diff_down)
+    H_ani = anis_field(anis_sam_T, m, ani_perp_sam)
+    H_th = th_field(m, m_squared, mmag_sam_T, Te, Tc_sam, chi_par_sam_T, under_tc, over_tc)
+
+    H_eff = H_ani + H_ex + H_th + H_ext
+
+    # and the damping parameters
+    alpha_par_sam_T = alpha_par_sample_T(mmag_sam_T, Te, alpha_par_sam, qs_sam_T, Tc_sam, under_tc, over_tc, lamda_sam)
+    alpha_trans_sam_T = alpha_trans_sample_T(mmag_sam_T, lamda_sam, Te, qs_sam_T, Tc_sam, under_tc, over_tc, lamda_sam)
+
+    # we will precompute the prefactors that are not of dimension len(sample)x3:
+
+    pref_trans = np.divide(alpha_trans_sam_T, m_squared)
+    pref_long = np.multiply(np.divide(alpha_par_sam_T, m_squared), np.einsum('ij,ij->i', m, H_eff))
+
+    # and all the cross products:
+    m_rot = np.cross(m, H_eff)  # precessional term
+    m_trans = np.cross(m, m_rot)  # transverse damping term
+
+    trans_damp = np.multiply(pref_trans[:, np.newaxis], m_trans)
+    long_damp = np.multiply(pref_long[:, np.newaxis], m)
+
+    # Now compute the magnetization increment
+    dm = gamma * dt * (-m_rot - trans_damp + long_damp)
+
+    return dm
+
+
+def run_LLB(tes, dt):
+    starttime = time.time()
+    # let's define some constants for simulation:
+    gamma = 1.76e11  # gyromagnetic ratio in (Ts)^{-1}
+    H_ext = np.array([[0, 0, 0] for _ in range(1)])  # external field in T
+
+    # load a sample and call the functions to get all parameters on the sample structure:
+    materials, sample, m_amp, m_phi, m_gamma, mat_gr_ind, mat_gr_ind_flat, mat_loc = get_sample()
+
+    Ms_sam = Ms_sample(sample)
+    # exch_coup_const_sam=get_exch_coup_sample(materials, sample, mat_loc)
+    Delta2_sam = Delta2_sample(sample)
+    S_sam = S_sample(sample)
+    Tc_sam = Tc_sample(sample)
+    J_sam = J_sample(sample)
+    lamda_sam = lamda_sample(sample)
+    muat_sam = muat_sample(sample)
+    ex_stiff_sam = get_ex_stiff_sample(materials, sample, mat_loc, Ms_sam, Delta2_sam)
+    K0_sam, kappa_ani_sam, ani_perp_sam = get_ani_sample(sample, Ms_sam)
+    alpha_par_sam = alpha_par_sample(sample)
+    qs_sam = qs_sample(sample)
+    dbrillouin_t1_sam = dbrillouin_t1_sample(sample)
+    dbrillouin_t2_sam = dbrillouin_t2_sample(sample)
+    chi_par_num_sam = chi_par_num_sample(sample)
+    chi_par_denomm1_sam = chi_par_denomm1_sample(sample)
+
+    # initialize the starting magnetization
+    # m=get_mag(np.array([m_amp, m_phi, m_gamma]))
+    m = np.array([[0.8, 0., 0.6]])
+    mag_map = [m]
+
+    initime = time.time()
+    print('Magnetization parameters initialized. Time spent:', str(initime - starttime), 's')
+
+    for i, Te in enumerate(tes):
+        dm1 = mag_incr(materials, sample, m_amp, m_phi, m_gamma, mat_gr_ind, mat_gr_ind_flat, mat_loc, Ms_sam,
+                       ex_stiff_sam, S_sam, Tc_sam, J_sam, lamda_sam, muat_sam, K0_sam, kappa_ani_sam, ani_perp_sam,
+                       alpha_par_sam, qs_sam, dbrillouin_t1_sam, dbrillouin_t2_sam, chi_par_num_sam,
+                       chi_par_denomm1_sam, m, Te, H_ext, gamma, dt)
+        m_heun = m + dm1
+        dm2 = mag_incr(materials, sample, m_amp, m_phi, m_gamma, mat_gr_ind, mat_gr_ind_flat, mat_loc, Ms_sam,
+                       ex_stiff_sam, S_sam, Tc_sam, J_sam, lamda_sam, muat_sam, K0_sam, kappa_ani_sam, ani_perp_sam,
+                       alpha_par_sam, qs_sam, dbrillouin_t1_sam, dbrillouin_t2_sam, chi_par_num_sam,
+                       chi_par_denomm1_sam, m_heun, Te, H_ext, gamma, dt)
+        newmag = m + np.divide((dm1 + dm2), 2)
+        m = newmag
+        mag_map.append(m)
+    endtime = time.time()
+    print('Magnetization map created. Total time spent:', str(endtime - starttime), 's')
+    return np.array(mag_map)
+
+mag_map=run_LLB(tes, 1e-16)
+
+color_data = mag_map[:, :, 1].T
+plt.imshow(color_data, cmap='hot', aspect='auto')
+plt.ylabel(r'grain position')
+plt.xlabel(r'time delay [ps]')
+plt.colorbar()
+plt.show()
